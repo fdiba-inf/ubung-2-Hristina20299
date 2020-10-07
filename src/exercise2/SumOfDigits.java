58244b492e42c876dd9098926e37f5a2c868c6f3
Hristina20299 <hristina.velkova.27@gmail.com>
2020-10-07 16:15:50 +0000

    -

--- src/exercise2/SumOfDigits.java
+++ src/exercise2/SumOfDigits.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class SumOfDigits {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    int a = 5;
-    int b = 6;
-    double sum = (a + b);
+    int a = 7;
+    int b = 8;
+    int c = 9;
+    double sum = (a + b + c);
     System.out.println("Sum of digits: " + sum);
   }
 }
